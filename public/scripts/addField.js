1cc65ff3e1f315537a375a1323f4f1eab9655cae
Reginaldo Santos <reginaldosdj@yahoo.com>
2020-08-08 23:04:02 -0300

    Acrescentando javascript e introdução a NODEJS

---
+++ public/scripts/addField.js
@@ -0,0 +1,19 @@
+//Script para adicionar o formulário de horários//
+
+
+document.querySelector("#add-time")
+.addEventListener('click', cloneField)
+
+function cloneField() {
+    
+   const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+   const fields = newFieldContainer.querySelectorAll('input')
+
+   fields.forEach(function(field){
+     field.value = ""
+
+   })
+
+document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
